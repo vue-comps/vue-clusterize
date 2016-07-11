fc832af897d146775c902ed7b5c3a4d33bc90e97
Paul Pflugradt <paulpflugradt@googlemail.com>
2016-07-11 13:52:49 +0200

    Updates

---
+++ presetRowHeight.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.container
+  a(href="https://vue-comps.github.io/vue-clusterize/blob/master/dev/basic.vue") source
+  clusterize(v-ref:clusterize v-bind:data="rowsData" v-bind:row-height="25" v-bind:height="400" v-bind:style="{width:'200px'}")
+    div.clusterize-row(v-bind:style="{height:height+'px'}") {{data}}
+</template>
+
+<script lang="coffee">
+module.exports =
+  components:
+    "clusterize": require "../src/clusterize.vue"
+  data: ->
+    rowsData: (x for x in [1..10000])
+</script>
+
+<style lang="stylus">
+.clusterize
+  border 1px solid black
+.container > a
+  position absolute
+  left 250px
+  top 40px
+</style>
